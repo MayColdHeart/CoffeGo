90a1fb57b331f5485c276dd691757383e834869d
MayColdHeart <devmayy@gmail.com>
2025-04-24 00:35:48 -0300

    Definindo TabBarIcon e definindo icones

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,29 @@
 import { Tabs } from 'expo-router';
-import { Image } from 'react-native';
+import { Image, StyleSheet } from 'react-native';
 
 export default function layoutTabs(){
     return (
         <Tabs screenOptions={{tabBarActiveTintColor: 'black'}}>
 
-            <Tabs.Screen name='index' options={{title: 'Home', tabBarIcon: ()=> <Image source={require('@/assets/images/icons/home.png')}/>}}  />
+            <Tabs.Screen name='menu' options={{title: 'Cardápio', tabBarIcon: ()=> <Image style={styles.icon} source={require('@/assets/images/icons/foodTruck.png')} />}}/>
 
-            <Tabs.Screen name='profile' options={{title: 'Profile'}}/>
+            <Tabs.Screen name='index' options={{title: 'Início', tabBarIcon: ()=> <Image style={styles.icon} source={require('@/assets/images/icons/home.png')}/>}}  />
 
-            <Tabs.Screen name='menu' options={{title: 'Cardápio'}}/>
-        
+            <Tabs.Screen name='profile' options={{title: 'Perfil', tabBarIcon: ()=> <Image style={styles.icon} source={require('@/assets/images/icons/profileIcon.png')} />}}/>
+                    
         </Tabs>
     )
 }
+
+
+
+
+const styles = StyleSheet.create({
+    container:  {
+        flex: 1,
+    },
+    icon:{
+        width: 30,
+        height: 30,
+    }
+})
