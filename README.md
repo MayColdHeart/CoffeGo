bd726a3efeed276808205306a163f3e580adb274
MayColdHeart <devmayy@gmail.com>
2025-04-25 01:56:06 -0300

    Troca de icones e mudança de título

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
  + props
+
+
+1 -> O nome do projeto deve ser alterado para CoffeGo
+
+Inicialmente deve ter uma lista de cafes
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,16 @@
 import { Tabs } from 'expo-router';
 import { Image, StyleSheet } from 'react-native';
 
-export default function layoutTabs(){
+export default function layoutTabs() {
     return (
-        <Tabs screenOptions={{tabBarActiveTintColor: 'black'}}>
+        <Tabs screenOptions={{ tabBarActiveTintColor: 'black' }}>
 
-            <Tabs.Screen name='menu' options={{title: 'Cardápio', tabBarIcon: ()=> <Image style={styles.icon} source={require('@/assets/images/icons/foodTruck.png')} />}}/>
+            <Tabs.Screen name='menu' options={{ title: 'Sabores de Cafe', tabBarIcon: () => <Image style={styles.icon} source={require('@/assets/images/icons/coffeCup.png')} /> }} />
 
-            <Tabs.Screen name='index' options={{title: 'Início', tabBarIcon: ()=> <Image style={styles.icon} source={require('@/assets/images/icons/home.png')}/>}}  />
+            <Tabs.Screen name='index' options={{ headerBackground: () => <Image style={styles.theme} source={require('@/assets/images/themes/coffeMain.jpg')} />, title: 'Coffe Go', tabBarIcon: () => <Image style={styles.icon} source={require('@/assets/images/icons/home.png')} /> }} />
 
-            <Tabs.Screen name='profile' options={{title: 'Perfil', tabBarIcon: ()=> <Image style={styles.icon} source={require('@/assets/images/icons/profileIcon.png')} />}}/>
-                    
+            <Tabs.Screen name='profile' options={{ title: 'Perfil', tabBarIcon: () => <Image style={styles.icon} source={require('@/assets/images/icons/profileIcon.png')} /> }} />
+
         </Tabs>
     )
 }
@@ -19,11 +19,19 @@ export default function layoutTabs(){
 
 
 const styles = StyleSheet.create({
-    container:  {
+    container: {
         flex: 1,
     },
-    icon:{
+    icon: {
         width: 30,
         height: 30,
-    }
+    },
+    theme: {
+        width: 400,
+        height: 60,
+
+    },
+
+
+
 })
